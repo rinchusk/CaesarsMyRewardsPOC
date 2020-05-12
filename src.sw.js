ade608bd141b6ff6adb1e566c9b45ec379e64a11
rkoshy <Rinchu.Koshy.contractor@hilton.com>
2020-05-12 16:01:48 -0500

    Updates

---
+++ src.sw.js
@@ -0,0 +1,39 @@
+importScripts("/precache-manifest.7cc350ff5c2e1b85a66d4ea77208ee54.js", "https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
+
+importScripts(
+  "https://cdnjs.cloudflare.com/ajax/libs/moment.js/2.24.0/moment.min.js"
+);
+console.log("Service worker Loaded ");
+workbox.precaching.precacheAndRoute(self.__precacheManifest);
+
+self.addEventListener("push", (e) => {
+  const data = e.data.json();
+  //const data =  e.data;
+  console.log("Push Received ");
+  console.log(data);
+  const { title, id, status, start, end } = data.data;
+  const notificationTitle = "New Offer : " + id + " Title : " + title;
+  const body =
+    "Start Date : " + formatDate(start) + " End Date : " + formatDate(end);
+
+  e.waitUntil(
+    self.registration.showNotification(notificationTitle, {
+      body: body
+    })
+  );
+});
+
+//self.addEventListener('install', () => self.skipWaiting());
+//self.addEventListener('activate', () => self.clients.claim());
+
+const formatDate = (dateToFormat) => {
+  try {
+    let formattedDate = new Date(dateToFormat);
+    formattedDate = moment(formattedDate).format("MMM-DD-YYYY");
+    return formattedDate;
+  } catch (e) {
+    console.log("Error in date conversion is " + e);
+    throw e;
+  }
+};
+
